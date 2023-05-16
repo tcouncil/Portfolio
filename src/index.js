da5aef24f5d8d322a239aacae8919b295d495b89
Travis Council <travis.council315@gmail.com>
2023-05-16 15:10:08 -0600

    update

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { HashRouter } from 'react-router-dom/cjs/react-router-dom.min';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
+    <Router basename='/'> 
+     
+        <App />
+
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
